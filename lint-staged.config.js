096b8ff3850afefb212cfec4429c2368eb851d46
Elad Chen <3628913+eladchen@users.noreply.github.com>
2024-08-16 17:43:54 +0300

    chore: cover changes in the tests folder

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,7 @@ import micromatch from 'micromatch';
 export default (allStagedFiles) => {
   const commands = [];
   const lintables = micromatch(allStagedFiles, ['**/*.js', '**/*.ts'], {});
-  const testables = micromatch(allStagedFiles, ['src/**'], {});
+  const testables = micromatch(allStagedFiles, ['src/**', 'tests/**'], {});
 
   if (lintables.length) {
     commands.push(`npm run lint:fix -- ${lintables.join(' ')}`);
